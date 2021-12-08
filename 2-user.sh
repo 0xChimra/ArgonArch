577c5af8ed02548da6ee865ca183ded125e41174
Blessed_NullArray <Blessed_NullArray@protonmail.com>
2021-12-08 18:49:42 +0100

    Test 2

--- 2-user.sh
+++ 2-user.sh
@@ -17,9 +17,7 @@ sudo pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
 sudo pacman-key --lsign-key FBA220DFC880C036
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 sudo -i
-echo "[chaotic-aur]" >> /etc/pacman.conf
-echo "Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
-exit
+sudo -i & echo "[chaotic-aur]" >> /etc/pacman.conf & echo "Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf & exit
 
 PKGS=(
 'autojump'

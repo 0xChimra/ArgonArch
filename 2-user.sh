e573f348df699c5e33b6b3c61fea8ee51afadec7
Blessed_NullArray <Blessed_NullArray@protonmail.com>
2021-12-08 17:15:29 +0100

    test

--- 2-user.sh
+++ 2-user.sh
@@ -13,8 +13,8 @@ mkdir -p ${HOME}/Documents/setup/zsh
 cp ${HOME}/ArgonArch/zshrc ${HOME}/.zshrc
 cp ${HOME}/ArgonArch/zalias ${HOME}/Documents/setup/zsh/zalias
 
-pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
-pacman-key --lsign-key FBA220DFC880C036
+sudo pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
+sudo pacman-key --lsign-key FBA220DFC880C036
 pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst' --noconfirm
 
 echo "[chaotic-aur]" >> /etc/pacman.conf
